5f3cf668c3aab035a312c6423e39c1557456a397
arthur-kerlan <arthur.mf.kerlan@gmail.com>
2026-06-24 15:34:20 -0300

    feat: inclusão de função num_lines()

--- include/csv.h
+++ include/csv.h
@@ -1,5 +1,11 @@
-//grava todas as estações em um arquivo CSV 
-int salvarCSV(int tam, stuct Estacao *v_est);
+#include "datatype_estacao.h"
+
+//grava todas as estações em arquivo CSV 
+int salvarCSV(int tam, struct Estacao v_est[]);
 
 //lê o arquivo CSV e restaura os dados para a memória.
+//
+//retorna no formato de array de struct Estacao
 struct Estacao* carregarCSV();
+
+int num_lines(char* path);
