d8975e62f6112faab74b0b080f7406255d9b7f84
arthur-kerlan <arthur.mf.kerlan@gmail.com>
2026-06-28 19:19:23 -0300

    refactor: Tratamento de exceção para função salvarCSV

--- src/main.c
+++ src/main.c
@@ -95,6 +95,7 @@ int main(int argc, char *argv[])
         exit_flag = 0;
         r = salvarCSV(tam, estacoes);
         if (r == 0) printf("Estacoes salvas automaticamente!\n");
+        else printf("Não foi possivel salvar alteracoes\n");
         free_est(tam, &estacoes);
         break;
       default:
